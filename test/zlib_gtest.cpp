d1dfed2c8a0c5ff5d944fcbd18a564cc180fc008
Neil Abcouwer <neil.abcouwer@jpl.nasa.gov>
2020-04-02 21:11:16 -0700

    Add test of recovering fulflush buffer with a corruption. Up to 80 percent coverage.

--- test/zlib_gtest.cpp
+++ test/zlib_gtest.cpp
@@ -67,7 +67,8 @@ typedef enum {
     SCENARIO_DICTIONARY = 1 << 0, // set and get dictionaries
     SCENARIO_PENDING = 1 << 1,    // do get pending
     SCENARIO_PARAMS = 1 << 2,     // adjust params
-    SCENARIO_FULL_FLUSH = 1 << 3, // adjust params
+    SCENARIO_FULL_FLUSH = 1 << 3, // flush in portions
+    SCENARIO_CORRUPT = 1 << 4, // corrupted portion
 } ZlibTestScenario;
 
 enum {NUM_CORPUS = 20}; //keep in sync
@@ -410,7 +411,7 @@ void zlib_test(
                                 max_in : (uInt) bytes_left_source;
                 bytes_left_source -= stream.avail_in;
             }
-            err = deflate(&stream, bytes_left_source ? Z_NO_FLUSH : Z_FINISH);
+            err = deflate(&stream, bytes_left_source ? flush_type : Z_FINISH);
         } while (err == Z_OK);
 
         if(cycles > 1) {
@@ -462,6 +463,21 @@ void zlib_test(
     printf("Compressed to size: %lu.\n",
             compressed_buf_len_out);
 
+
+//    printf("Compressed:\n");
+//    for (int i = 0; i < compressed_buf_len_out; i++) {
+//        if (i % 40 == 0) {
+//            printf("\n");
+//        }
+//        printf("%02x ", compressed_buf[i]);
+//    }
+//    printf("\n");
+
+    if(scenarios & SCENARIO_CORRUPT) {
+        printf("Corrupting compressed buffer\n");
+        compressed_buf[42]++;
+    }
+
     uLong uncompressed_buf_len = source_buf_len;
     Byte * uncompressed_buf = (Byte *) malloc(uncompressed_buf_len);
     EXPECT_NE(uncompressed_buf, (Byte * )NULL);
@@ -474,6 +490,8 @@ void zlib_test(
 
     //compressed_buf_len_out = compressed_buf_len;
     uLong uncompressed_buf_len_out = uncompressed_buf_len;
+
+
 
     if (scenarios == SCENARIO_BASIC) {
         switch (wrapper) {
@@ -517,69 +535,154 @@ void zlib_test(
         mem_inf.workLen = work_buf_len;
         mem_inf.workAlloced = 0;
 
-        z_stream stream_inf;
-        stream_inf.next_in = (z_const Bytef *) compressed_buf;
-        stream_inf.avail_in = compressed_buf_len;
-        stream_inf.next_out = uncompressed_buf;
-        stream_inf.avail_out = uncompressed_buf_len;
-        stream_inf.zalloc = z_static_alloc;
-        stream_inf.zfree = z_static_free;
-        stream_inf.opaque = (voidpf) &mem_inf;
+        z_stream stream;
 
-        err = inflateInit(&stream_inf);
+        uInt max_in = (uInt)-1;
+        uInt max_out = (uInt)-1;
+        uLong bytes_left_dest =  uncompressed_buf_len;
+        uLong bytes_left_source =  compressed_buf_len;
+
+//
+//        stream_inf.next_in = (z_const Bytef *) compressed_buf;
+//          stream_inf.avail_in = compressed_buf_len;
+//          stream_inf.next_out = uncompressed_buf;
+//          stream_inf.avail_out = uncompressed_buf_len;
+
+        stream.next_in = (z_const Bytef *) compressed_buf;
+        stream.avail_in = 0;
+        stream.next_out = uncompressed_buf;
+        stream.avail_out = 0;
+
+
+        stream.zalloc = z_static_alloc;
+        stream.zfree = z_static_free;
+        stream.opaque = (voidpf) &mem_inf;
+
+        err = inflateInit(&stream);
         EXPECT_EQ(err, Z_OK);
 
-        if(scenarios & SCENARIO_DICTIONARY) {
-            // inflate should report dictionary needed
-            err = inflate(&stream_inf, Z_NO_FLUSH);
-            EXPECT_EQ(err, Z_NEED_DICT);
-            if (stream_inf.msg != Z_NULL) {
-                printf("msg: %s\n", stream_inf.msg);
-            }
-            printf("setting dictionary\n");
-            err = inflateSetDictionary(&stream_inf, dictionary, dictLength);
-            EXPECT_EQ(err, Z_OK);
-            if (stream_inf.msg != Z_NULL) {
-                printf("msg: %s\n", stream_inf.msg);
-            }
-        }
+//        if(scenarios & SCENARIO_DICTIONARY) {
+//            // inflate should report dictionary needed
+//            err = inflate(&stream, Z_NO_FLUSH);
+//            EXPECT_EQ(err, Z_NEED_DICT);
+//            if (stream.msg != Z_NULL) {
+//                printf("msg: %s\n", stream.msg);
+//            }
+//            printf("setting dictionary\n");
+//            err = inflateSetDictionary(&stream, dictionary, dictLength);
+//            EXPECT_EQ(err, Z_OK);
+//            if (stream.msg != Z_NULL) {
+//                printf("msg: %s\n", stream.msg);
+//            }
+//        }
 
-        // inflate
-        err = inflate(&stream_inf, Z_FINISH);
-        uncompressed_buf_len_out = stream_inf.total_out;
-        EXPECT_EQ(err, Z_STREAM_END);
-        if (stream_inf.msg != Z_NULL) {
-            printf("msg: %s\n", stream_inf.msg);
+//        // inflate
+//        err = inflate(&stream, Z_FINISH);
+//        uncompressed_buf_len_out = stream.total_out;
+//        EXPECT_EQ(err, Z_STREAM_END);
+//        if (stream.msg != Z_NULL) {
+//            printf("msg: %s\n", stream.msg);
+//        }
+
+
+        do {
+            if (stream.avail_out == 0) {
+                stream.avail_out =
+                        bytes_left_dest > (uLong) max_out ?
+                                max_out : (uInt) bytes_left_dest;
+                bytes_left_dest -= stream.avail_out;
+            }
+            if (stream.avail_in == 0) {
+                stream.avail_in =
+                        bytes_left_source > (uLong) max_in ?
+                                max_in : (uInt) bytes_left_source;
+                bytes_left_source -= stream.avail_in;
+            }
+            printf("before: stream.avail_in = %d. stream.avail_out = %d.\n",
+                    stream.avail_in, stream.avail_out);
+            err = inflate(&stream, Z_NO_FLUSH);
+            printf("after:  stream.avail_in = %d. stream.avail_out = %d.\n",
+                    stream.avail_in, stream.avail_out);
+
+            if (err == Z_NEED_DICT) {
+                EXPECT_TRUE(scenarios & SCENARIO_DICTIONARY);
+                printf("setting dictionary\n");
+                err = inflateSetDictionary(&stream, dictionary, dictLength);
+                EXPECT_EQ(err, Z_OK);
+                if (stream.msg != Z_NULL) {
+                    printf("msg: %s\n", stream.msg);
+                }
+            }
+
+            if (err == Z_DATA_ERROR) {
+                // try to find a new flush point
+                err = inflateSync(&stream);
+                printf("data error. inflateSync returned %d\n", err);
+                printf("stream.avail_in = %d. stream.avail_out = %d.\n",
+                        stream.avail_in, stream.avail_out);
+            }
+
+        } while (err == Z_OK);
+
+        if(scenarios & SCENARIO_CORRUPT) {
+            EXPECT_EQ(err, Z_DATA_ERROR);
+        } else {
+            EXPECT_EQ(err, Z_STREAM_END);
         }
+        uncompressed_buf_len_out = stream.total_out;
 
         if(scenarios & SCENARIO_DICTIONARY) {
             Bytef * dictionary_out_inf = (Bytef *) malloc(32768);
             ASSERT_NE(dictionary_out_inf, (Bytef*)NULL);
             uInt dict_out_inf_len = 0;
             printf("getting dictionary\n");
-            err = inflateGetDictionary(&stream_inf, dictionary_out_inf,
+            err = inflateGetDictionary(&stream, dictionary_out_inf,
                     &dict_out_inf_len);
             EXPECT_EQ(err, Z_OK);
             free(dictionary_out_inf);
-
         }
 
-        err = inflateEnd(&stream_inf);
+        err = inflateEnd(&stream);
         EXPECT_EQ(err, Z_OK);
 
     } // end if not basic
 
-    EXPECT_EQ(uncompressed_buf_len_out, source_buf_len);
 
     // Compare
-    int nbad = 0;
-    if (uncompressed_buf_len_out == source_buf_len) {
-        for (int i = 0; i < source_buf_len; i++) {
-            if (source_buf[i] != uncompressed_buf[i])
-                nbad++;
+    if (!(scenarios & SCENARIO_CORRUPT)) {
+        EXPECT_EQ(uncompressed_buf_len_out, source_buf_len);
+
+        int nbad = 0;
+        if (uncompressed_buf_len_out == source_buf_len) {
+            for (int i = 0; i < source_buf_len; i++) {
+                if (source_buf[i] != uncompressed_buf[i])
+                    nbad++;
+            }
         }
+        EXPECT_EQ(nbad, 0);
+    } else {
+        printf("Input:\n");
+        for (int i = 0; i < (source_buf_len < 240 ? source_buf_len : 240); i++) {
+            if (isprint(source_buf[i]) || source_buf[i] == 0xA
+                || source_buf[i] == 0xD) {
+                printf("%c", source_buf[i]);
+            } else {
+                printf(" %#x ", source_buf[i]);
+            }
+        }
+        printf("...\n");
+        printf("Output:\n");
+        for (int i = 0; i < (uncompressed_buf_len_out < 240 ? uncompressed_buf_len_out : 240); i++) {
+            if (isprint(uncompressed_buf[i]) || uncompressed_buf[i] == 0xA
+                || uncompressed_buf[i] == 0xD) {
+                printf("%c", uncompressed_buf[i]);
+            } else {
+                printf(" %#x ", uncompressed_buf[i]);
+            }
+        }
+        printf("...\n");
     }
-    EXPECT_EQ(nbad, 0);
+
 
 //    EXPECT_EQ(head.time, head_out.time);
 //    EXPECT_EQ(0, strcmp((char*)head_out.name, (char*)head.name));
@@ -754,6 +857,14 @@ TEST_F(ZlibTest, FullFlush) {
     zlib_test_alice(WRAP_ZLIB, SCENARIO_FULL_FLUSH);
 }
 
+TEST_F(ZlibTest, Corrupt) {
+    zlib_test_alice(WRAP_ZLIB, SCENARIO_CORRUPT);
+}
+
+TEST_F(ZlibTest, FullFlushCorrupt) {
+    zlib_test_alice(WRAP_ZLIB, SCENARIO_FULL_FLUSH | SCENARIO_CORRUPT);
+}
+
 TEST_F(ZlibTest, CompressSafeErrors) {
     printf("test errors in compress_safe\n");
 
@@ -914,87 +1025,6 @@ TEST_F(ZlibTest, UncompressSafeErrors) {
                             work_buf, work_buf_len, DEF_WBITS, &header);
     EXPECT_EQ(err, Z_STREAM_ERROR);
     printf("zError:%s\n", zError(err));
-
-
-//    // repeat with NULL GIP HEADER
-//
-//    free(uncompressed_buf);
-//    free(compressed_buf);
-//    free(source_buf);
-//    free(work_buf);
-//
-//    file = fopen("corpus/cantrbry/alice29.txt", "r");
-//    ASSERT_FALSE(file == NULL);
-//
-//    source_buf_len = CORPUS_MAX_SIZE;
-//    source_buf = (Byte *) malloc(source_buf_len);
-//    nread = fread(source_buf, 1, source_buf_len, file);
-//    ASSERT_FALSE(ferror(file));
-//    fclose(file);
-//    ASSERT_TRUE(nread <= source_buf_len);
-//    source_buf_len = nread;
-//
-//
-//    err = compressGetMaxOutputSize(source_buf_len, &compressed_buf_len);
-//    EXPECT_EQ(err, Z_OK);
-//    compressed_buf = (Byte *) malloc(compressed_buf_len);
-//    ASSERT_NE(compressed_buf, (Bytef*)NULL);
-//
-//
-//    err = compressGetMinWorkBufSize(&work_buf_len);
-//    EXPECT_EQ(err, Z_OK);
-//    work_buf = (Byte *) malloc(work_buf_len);
-//    ASSERT_NE(work_buf, (Bytef*)NULL);
-//    compressed_buf_len_out = compressed_buf_len;
-//
-//    err = compressSafeGzip(compressed_buf, &compressed_buf_len_out,
-//            source_buf, source_buf_len,
-//            work_buf, work_buf_len, Z_DEFAULT_COMPRESSION, Z_NULL);
-//    EXPECT_EQ(err, Z_OK);
-//
-//    free(work_buf);
-//
-//    uncompressed_buf_len = source_buf_len;
-//    uncompressed_buf = (Byte *) malloc(uncompressed_buf_len);
-//    EXPECT_NE(uncompressed_buf, (Byte * )NULL);
-//    err = uncompressGetMinWorkBufSize(&work_buf_len);
-//    EXPECT_EQ(err, Z_OK);
-//    work_buf = (Byte *) malloc(work_buf_len);
-//
-//
-//    printf("null out header\n");
-//    compressed_buf_len_out2 = compressed_buf_len_out;
-//    uncompressed_buf_len_out = uncompressed_buf_len;
-//    err = uncompressSafeGzip(uncompressed_buf, &uncompressed_buf_len_out,
-//                            compressed_buf, &compressed_buf_len_out2,
-//                            work_buf, work_buf_len, Z_NULL);
-//    EXPECT_EQ(err, Z_MEM_ERROR);
-//    printf("zError:%s\n", zError(err));
-//
-//    printf("bad window bits arg\n");
-//    compressed_buf_len_out2 = compressed_buf_len_out;
-//    uncompressed_buf_len_out = uncompressed_buf_len;
-//    err = uncompressSafe2(uncompressed_buf, &uncompressed_buf_len_out,
-//                            compressed_buf, &compressed_buf_len_out2,
-//                            work_buf, work_buf_len, 500);
-//    EXPECT_EQ(err, Z_STREAM_ERROR);
-//    printf("zError:%s\n", zError(err));
-//
-//    printf("output buf size small\n");
-//    compressed_buf_len_out2 = compressed_buf_len_out;
-//    uncompressed_buf_len_out = 42;
-//    err = uncompressSafe(uncompressed_buf, &uncompressed_buf_len_out,
-//                            compressed_buf, &compressed_buf_len_out2,
-//                            work_buf, work_buf_len);
-//    EXPECT_EQ(err, Z_BUF_ERROR);
-//    printf("zError:%s\n", zError(err));
-//
-//
-//
-//    free(uncompressed_buf);
-//    free(compressed_buf);
-//    free(source_buf);
-//    free(work_buf);
 
 
 
